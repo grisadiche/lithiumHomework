47645e22084fea2830ee7f0cd82e49ba38ece03e
Grisadiche <grisadiche@gmail.com>
2018-09-12 12:42:25 -0500

    Work out the exceptions for no seraching value - return false when rowArrary[6] is out of bounds

--- lithiumHomework/src/Person.java
+++ lithiumHomework/src/Person.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -11,18 +12,25 @@ public class Person {
         private String state;
         private int zipCode;
         private String industry;
-        //private boolean searching;                     //add in boolean
+        private boolean searching;                     //add in boolean
 
 
-    public Person(String name, int age, String city, String state, int zipCode, String industry){
+    public Person(String name, int age, String city, String state, int zipCode, String industry, Boolean searching){
         this.name = name;
         this.age = age;
         this.city = city;
         this.state = state;
         this.zipCode = zipCode;
         this.industry = industry;
-    }  // removed ,boolean searching
+        this.searching = searching;
+    }
 
+    public static Boolean toBooleanObject(String str) {
+        if ("yes".equalsIgnoreCase(str)) {
+            return true;
+        }
+        return false;
+    }
 
     //getters
     public String getName(){return this.name;}
@@ -31,47 +39,51 @@ public class Person {
     public String getState(){return state;}
     public int getZipCode(){return zipCode;}
     public String getIndustry(){return industry;}
-    //public String getSearching(){return searching;}
+    public Boolean getSearching(){return searching;}
 
+    public String toString(){                                           //replaces the hashcode object output with my own. not scalable to new fields
+        return "Person[" + name + "," + age + "," + city + "," + state + "," + zipCode + "," + industry + "," + searching + "]";
+    }
 
 
     public static void main(String[] args) {
         String secondCSV = ("Name,Age,City,State,Zip,Industry,Job Searching?\n" +
                 "John,23,Austin,TX,78730,Software,Yes\n" +
-                "Karen,22,Austin,TX,78730,Finance,No\n" +
+                "Karen,22,Austin,TX,78730,,No\n" +
                 "Bob,35,San Francsico,CA,94501,Finance,Yes\n" +
-                "Thomas,43,Austin,TX,78730,Software, \n" +
+                "Thomas,43,Austin,TX,78730,Software,No\n" +
                 "Jennifer,34,San Francsico,CA,94501,Finance,No\n" +
-                "Lucy,55,Austin,TX,78730,Software,No\n" +
-                "Rob,23,San Francsico,CA,94501, ,No\n" +
-                "Margaret,27,Austin,TX,78730,Finance,\n" +
-                "John,66,Dallas,TX,75001, , \n" +
-                "Mary,27,San Francsico,CA,94501,Accounting, \n" +
+                "Lucy,55,Austin,TX,78730,Software, \n" +
+                "Rob,23,San Francsico,CA,94501,,No\n" +
+                "Margaret,27,Austin,TX,78730,Finance,No\n" +
+                "John,66,Dallas,TX,75001,,No \n" +
+                "Mary,27,San Francsico,CA,94501,Accounting,No\n" +
                 "Mary,29,San Francsico,CA,94501,Software,No\n" +
-                "George,36,Austin,TX,78746,Software, \n" +
+                "George,36,Austin,TX,78746,Software,No\n" +
                 "Michael,37,Austin,TX,78746,HR,Yes\n" +
                 "Larry,34,Austin,TX,78746,Finance,Yes\n" +
-                "Brian,31,Austin,TX,78746, , \n" +
-                "Elon,31,Austin,TX,78746, , \n" +
+                "Brian,31,Austin,TX,78746,,No\n" +
+                "Elon,31,Austin,TX,78746,,No\n" +
                 "Robert,31,Austin,TX,78746,Finance,Yes\n" +
-                "Bill,31,Austin,TX,78746,Finance, \n" +
-                "Emma,31,Austin,TX,78746,Finance,Yes\n" +
-                "Liz,31,Austin,TX,78746, ,No\n" +
+                "Bill,31,Austin,TX,78746,Finance,No\n" +
+                "Emma,31,Austin,TX,78746,Finance,No\n" +
+                "Liz,31,Austin,TX,78746,,No\n" +
                 "Ronald,43,Dallas,TX,75001,HR,No\n" +
-                "Rebecca,35,Houston,TX,77473,Finance, ");
+                "Rebecca,35,Houston,TX,77473,Finance,No");
 
         String[] separateArray = secondCSV.split("\n");             //separate string by line
         ArrayList<Person> objectArray = new ArrayList<>();                //new arraylist of Person objects
 
         for(int i = 1; i < separateArray.length; i++) {
-            String[] rowArrays = separateArray[i].split(",");       //splits each row at comma (adjust for nulls)
+            String[] rowArrays = separateArray[i].split(",");//splits each row at comma (adjust for nulls?)
             objectArray.add(new Person(                                    //creates new person in my Person arraylist
                     rowArrays[0],                                          //names
                     Integer.parseInt(rowArrays[1]),                        //ages
                     rowArrays[2],                                          //cities
                     rowArrays[3],                                          //states
                     Integer.parseInt(rowArrays[4]),                        //zips
-                    rowArrays[5]));                                        //industry
+                    rowArrays[5],                                          //industry
+                    toBooleanObject(rowArrays[6])));                       //uses method to parse booleans
         }
 
         HashMap<String, Integer> stateCountMap = new HashMap<String, Integer>(); //new hashmap for state counts
@@ -134,16 +146,18 @@ public class Person {
 
 
        ArrayList<Integer> ageArray = new ArrayList<>();
-        for(int i = 1; i < separateArray.length - 1; i++){
-            ageArray.add(objectArray.get(i-1).getAge());
+        for(int i = 0; i < objectArray.size(); i++){
+            ageArray.add(objectArray.get(i).getAge());
         }
        int ageMax = Collections.max(ageArray);
        int ageMaxIndex = ageArray.indexOf(ageMax);
        int ageMin = Collections.min(ageArray);
        int ageMinIndex = ageArray.indexOf(ageMin);
 
-System.out.println("The oldest person is: " + separateArray[ageMaxIndex + 1]);
-System.out.println("The youngest person is: " + separateArray[ageMinIndex + 1]);
+System.out.println("The oldest person is: " + objectArray.get(ageMaxIndex));
+System.out.println("The youngest person is: " + objectArray.get(ageMinIndex));
+
+
 
         }
     }
