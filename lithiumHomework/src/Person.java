c4eb41d271b1243aba39c20f9c42ad2b04818d1d
grisadiche <grisadiche@gmail.com>
2018-10-03 18:56:57 -0600

    added in some nifty stuff.

--- lithiumHomework/src/Person.java
+++ lithiumHomework/src/Person.java
@@ -1,18 +1,16 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 import java.util.Map.Entry;
+import java.util.function.Predicate;
 
 public class Person {
 
-        private String name;
-        private int age;
-        private String city;
-        private String state;
-        private int zipCode;
-        private String industry;
-        private boolean searching;                     //add in boolean
+    private String name;
+    private int age;
+    private String city;
+    private String state;
+    private int zipCode;
+    private String industry;
+    private boolean searching;                     //add in boolean
 
 
     /*public Person(String name, int age, String city, String state, int zipCode, String industry, Boolean searching){
@@ -26,8 +24,8 @@ public class Person {
     }
 */
     //setters
-    public void setName(String name){
-        if(name.length() == 0) {
+    public void setName(String name) {
+        if (name.length() == 0) {
             this.name = "unknown";
         } else if (name == null) {
             this.name = "unknown";
@@ -35,8 +33,9 @@ public class Person {
             this.name = name;
         }
     }
-    public void setAge(String age){
-        if(age.length() == 0) {
+
+    public void setAge(String age) {
+        if (age.length() == 0) {
             this.age = 0;
         } else if (age == null) {
             this.age = 0;
@@ -44,8 +43,9 @@ public class Person {
             this.age = Integer.parseInt(age);
         }
     }
-    public void setCity(String city){
-        if(city.length() == 0) {
+
+    public void setCity(String city) {
+        if (city.length() == 0) {
             this.city = "unknown";
         } else if (city == null) {
             this.city = "unknown";
@@ -53,8 +53,9 @@ public class Person {
             this.city = city;
         }
     }
-    public void setState(String state){
-        if(state.length() == 0) {
+
+    public void setState(String state) {
+        if (state.length() == 0) {
             this.state = "unknown";
         } else if (state == null) {
             this.state = "unknown";
@@ -62,8 +63,9 @@ public class Person {
             this.state = state;
         }
     }
-    public void setZipCode(String zipCode){
-        if(zipCode.length() == 0) {
+
+    public void setZipCode(String zipCode) {
+        if (zipCode.length() == 0) {
             this.zipCode = 0;
         } else if (zipCode == null) {
             this.zipCode = 0;
@@ -71,8 +73,9 @@ public class Person {
             this.zipCode = Integer.parseInt(zipCode);
         }
     }
-    public void setIndustry(String industry){
-        if(industry.length() == 0) {
+
+    public void setIndustry(String industry) {
+        if (industry.length() == 0) {
             this.industry = "unknown";
         } else if (industry == null) {
             this.industry = "unknown";
@@ -80,8 +83,9 @@ public class Person {
             this.industry = industry;
         }
     }
-    public void setSearching(String searching){
-        if(searching.length() == 0) {
+
+    public void setSearching(String searching) {
+        if (searching.length() == 0) {
             this.searching = false;
         } else if (searching == null) {
             this.searching = false;
@@ -92,69 +96,135 @@ public class Person {
         }
     }
 
+    public static ArrayList<String> nameArray = new ArrayList<>();
+    public static ArrayList<String> ageArray = new ArrayList<>();
+    public static ArrayList<String> cityArray = new ArrayList<>();
+    public static ArrayList<String> stateArray = new ArrayList<>();
+    public static ArrayList<String> zipCodeArray = new ArrayList<>();
+    public static ArrayList<String> industryArray = new ArrayList<>();
+    public static ArrayList<String> searchingArray = new ArrayList<>();
 
-    //getters
-    public String getName(){return this.name;}
-    public int getAge(){return age;}
-    public String getCity(){return city;}
-    public String getState(){return state;}
-    public int getZipCode(){return zipCode;}
-    public String getIndustry(){return industry;}
-    public Boolean getSearching(){return searching;}
-
-    public String toString(){                                           //replaces the hashcode object output with my own. not scalable to new fields
-        return "[" + name + "," + age + "," + city + "," + state + "," + zipCode + "," + industry + "," + searching + "]";
+    public static void createDatabase(ArrayList<Person> masterArray) {
+        for (int i = 0; i < masterArray.size(); i++) {
+            nameArray.add(masterArray.get(i).getName());
+            ageArray.add(Integer.toString(masterArray.get(i).getAge()));
+            cityArray.add(masterArray.get(i).getCity());
+            stateArray.add(masterArray.get(i).getState());
+            zipCodeArray.add(Integer.toString(masterArray.get(i).getZipCode()));
+            industryArray.add(masterArray.get(i).getIndustry());
+            searchingArray.add(String.valueOf(masterArray.get(i).getSearching()));
+        }
     }
 
-
-    public static void main(String[] args) {
-        String secondCSV = ("Name,Age,City,State,Zip,Industry,Job Searching?\n" +
-                "John,23,Austin,TX,78730,Software,Yes\n" +
-                "Karen,22,Austin,TX,78730,Finance,No\n" +
-                "Bob,35,San Francsico,CA,94501,Finance,Yes\n" +
-                "Thomas,43,Austin,TX,78730,Software,\n" +
-                "Jennifer,34,San Francsico,CA,94501,Finance,No\n" +
-                "Lucy,55,Austin,TX,78730,Software,No\n" +
-                "Rob,23,San Francsico,CA,94501,,No\n" +
-                "Margaret,27,Austin,TX,78730,Finance,\n" +
-                "John,66,Dallas,TX,75001,,\n" +
-                "Mary,27,San Francsico,CA,94501,Accounting,\n" +
-                "Mary,29,San Francsico,CA,94501,Software,No\n" +
-                "George,36,Austin,TX,78746,Software,\n" +
-                "Michael,37,Austin,TX,78746,HR,Yes\n" +
-                "Larry,34,Austin,TX,78746,Finance,Yes\n" +
-                "Brian,31,Austin,TX,78746,,\n" +
-                "Elon,31,Austin,TX,78746,,\n" +
-                "Robert,31,Austin,TX,78746,Finance,Yes\n" +
-                "Bill,31,Austin,TX,78746,Finance,\n" +
-                "Emma,31,Austin,TX,78746,Finance,Yes\n" +
-                "Liz,31,Austin,TX,78746,,No\n" +
-                "Ronald,43,Dallas,TX,75001,HR,No\n" +
-                "Rebecca,35,Houston,TX,77473,Finance,");
-
-        String[] separateArray = secondCSV.split("\n");             //separate string by line
-        ArrayList<Person> objectArray = new ArrayList<>();                //new arraylist of Person objects
-        String [] fixedLengthRowArray = new String[7];                    //fixed length array to handle missing data
-
-        for(int i = 1; i < separateArray.length; i++) {                   //loop through each row
-            String [] rowArrays = separateArray[i].split(",");       //splits each row at commas
-
-            for(int j = 0; j < rowArrays.length; j++) {                     //loop through length of row
-                fixedLengthRowArray[j] = rowArrays[j];                      //adds data to fixed length array
+    private static ArrayList<Person> filter(ArrayList<Person> personArray, String filterKey, String filterValue) {
+        ArrayList<Person> returnArray = new ArrayList<>(personArray);
+        Predicate<Person> valueFilter = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return false;
             }
-            objectArray.add(new Person());                                    //creates new person in my Person arraylist
-                objectArray.get(i-1).setName(fixedLengthRowArray[0]);                                          //names
-                objectArray.get(i-1).setAge(fixedLengthRowArray[1]);                                           //ages
-                objectArray.get(i-1).setCity(fixedLengthRowArray[2]);                                          //cities
-                objectArray.get(i-1).setState(fixedLengthRowArray[3]);                                          //states
-                objectArray.get(i-1).setZipCode(fixedLengthRowArray[4]);                                       //zips
-                objectArray.get(i-1).setIndustry(fixedLengthRowArray[5]);                                       //industry
-                objectArray.get(i-1).setSearching(fixedLengthRowArray[6]);                                    //searching?
+        };
+        switch (filterKey) {
+            case "name":
+                valueFilter = person -> !person.getName().equalsIgnoreCase(filterValue);
+                break;
+            case "age":
+                valueFilter = person -> !Integer.toString(person.getAge()).equalsIgnoreCase(filterValue);
+                break;
+            case "city":
+                valueFilter = person -> !person.getCity().equalsIgnoreCase(filterValue);
+                break;
+            case "state":
+                valueFilter = person -> !person.getState().equalsIgnoreCase(filterValue);
+                break;
+            case "zipCode":
+                valueFilter = person -> !Integer.toString(person.getZipCode()).equalsIgnoreCase(filterValue);
+                break;
+            case "industry":
+                valueFilter = person -> !person.getIndustry().equalsIgnoreCase(filterValue);
+                break;
+            case "searching":
+                valueFilter = person -> !String.valueOf(person.getSearching()).equalsIgnoreCase(filterValue);
+                break;
+            default:
+                System.out.println("Please choose one of the categories: name, age, city, state, zipcode, industry, or searching");
+                break;
+
         }
 
+        returnArray.removeIf(valueFilter);
+        return returnArray;
+    }
 
-        HashMap<String, Integer> stateCountMap = new HashMap<String, Integer>(); //new hashmap for state counts
-        ArrayList<String> states = new ArrayList<>();                            //new arraylist of states
+    public static void getPopulation(ArrayList<Person> personArray) {
+
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Which value type would you like a list of? (name, age, city, state, zipcode, industry, searching)");
+        String listValue = reader.nextLine();
+        getCount(personArray, listValue);
+        System.out.println("Which value would you like to sort by? Choose a specific value from the list above");
+        String categoryValue = reader.nextLine();
+        reader.close();
+
+        System.out.println(filter(personArray, listValue, categoryValue));
+    }
+
+    public static void getCount(ArrayList<Person> personArray, String searchString) {
+        HashMap<String, Integer> mapCount = new HashMap<String, Integer>();
+
+        for (int i = 0; i < personArray.size(); i++) {
+            if (searchString.equalsIgnoreCase("name")) {
+                if (!mapCount.containsKey(nameArray.get(i))) {
+                    mapCount.put(nameArray.get(i), 1);
+                } else {
+                    mapCount.put(nameArray.get(i), mapCount.get(nameArray.get(i)) + 1);
+                }
+            } else if (searchString.equalsIgnoreCase("age")) {
+                if (!mapCount.containsKey(ageArray.get(i))) {
+                    mapCount.put(ageArray.get(i), 1);
+                } else {
+                    mapCount.put(ageArray.get(i), mapCount.get(ageArray.get(i)) + 1);
+                }
+            } else if (searchString.equalsIgnoreCase("city")) {
+                if (!mapCount.containsKey(cityArray.get(i))) {
+                    mapCount.put(cityArray.get(i), 1);
+                } else {
+                    mapCount.put(cityArray.get(i), mapCount.get(cityArray.get(i)) + 1);
+                }
+
+            } else if (searchString.equalsIgnoreCase("state")) {
+                if (!mapCount.containsKey(stateArray.get(i))) {
+                    mapCount.put(stateArray.get(i), 1);
+                } else {
+                    mapCount.put(stateArray.get(i), mapCount.get(stateArray.get(i)) + 1);
+                }
+            } else if (searchString.equalsIgnoreCase("zipcode")) {
+                if (!mapCount.containsKey(zipCodeArray.get(i))) {
+                    mapCount.put(zipCodeArray.get(i), 1);
+                } else {
+                    mapCount.put(zipCodeArray.get(i), mapCount.get(zipCodeArray.get(i)) + 1);
+                }
+            } else if (searchString.equalsIgnoreCase("industry")) {
+                if (!mapCount.containsKey(industryArray.get(i))) {
+                    mapCount.put(industryArray.get(i), 1);
+                } else {
+                    mapCount.put(industryArray.get(i), mapCount.get(industryArray.get(i)) + 1);
+                }
+
+            } else if (searchString.equalsIgnoreCase("searching")) {
+                if (!mapCount.containsKey(searchingArray.get(i))) {
+                    mapCount.put(searchingArray.get(i), 1);
+                } else {
+                    mapCount.put(searchingArray.get(i), mapCount.get(searchingArray.get(i)) + 1);
+                }
+            } else {
+                System.out.println("error");
+            }
+        }
+        System.out.println(mapCount);
+    }
+/*
         for(int i = 0; i < objectArray.size(); i++) {
             states.add(objectArray.get(i).getState());                           //adds states from objects
             if (!stateCountMap.containsKey(states.get(i))) {                     //check for 1st instance
@@ -164,68 +234,44 @@ public class Person {
             }
         }
 
-        int maxValueStates = Collections.max(stateCountMap.values());       //determines the highest value in statecounts map
-        ArrayList<String> maxStates = new ArrayList<>();                    //a list in case there are multiple maxes
-        for (Entry<String, Integer> entry : stateCountMap.entrySet()) {
-            if (entry.getValue()==maxValueStates) {          // compares each value against max value
-                maxStates.add(entry.getKey());               // add most populous state(s) to maxStates ArrayList
-            }
-        }
+    public void getList()
+    //public void getCount(ArrayList){
 
-        ArrayList<String> maxStatePop = new ArrayList<>();              //max state population list
-        for (int i = 0; i < separateArray.length - 1; i++) {
-            if(maxStates.contains(objectArray.get(i).getState())) {     //looks for states in maxStates array
-                maxStatePop.add(objectArray.get(i).getName());          //adds the name for each from that state
-            }
-        }
+    //}
+*/
 
-        System.out.println("The counts for each state are: " + stateCountMap);
-        System.out.println("The most populous state(s): " + maxStates);
-        System.out.println("The following people reside in " + maxStates + ": " + maxStatePop);
-
-
-
-        HashMap<Integer, Integer> zipCountMap = new HashMap<Integer, Integer>();        //same as above for zips
-        ArrayList<Integer> zips = new ArrayList<>();                                    //create method?
-        for(int i = 0; i < objectArray.size(); i++) {
-            zips.add(objectArray.get(i).getZipCode());
-            if (!zipCountMap.containsKey(zips.get(i))) {
-                zipCountMap.put(zips.get(i), 1);
-            } else {
-                zipCountMap.put(zips.get(i), zipCountMap.get(zips.get(i)) + 1);
-            }
-        }
-
-        System.out.println("Zip codes by frequency: " + zipCountMap);
-
-        HashMap<String, Integer> cityCountMap = new HashMap<String, Integer>();         // same but for cities
-        ArrayList<String> cities = new ArrayList<>();                                   // make a method
-        for(int i = 0; i < objectArray.size(); i++) {
-            cities.add(objectArray.get(i).getCity());
-            if (!cityCountMap.containsKey(cities.get(i))) {
-                cityCountMap.put(cities.get(i), 1);
-            } else {
-                cityCountMap.put(cities.get(i), cityCountMap.get(cities.get(i)) + 1);
-            }
-        }
-
-        System.out.println("The following cities are represented: " + cityCountMap.keySet());
-
-
-       ArrayList<Integer> ageArray = new ArrayList<>();
-        for(int i = 0; i < objectArray.size(); i++){
-            ageArray.add(objectArray.get(i).getAge());
-        }
-       int ageMax = Collections.max(ageArray);
-       int ageMaxIndex = ageArray.indexOf(ageMax);
-       int ageMin = Collections.min(ageArray);
-       int ageMinIndex = ageArray.indexOf(ageMin);
-
-System.out.println("The oldest person is: " + objectArray.get(ageMaxIndex));
-System.out.println("The youngest person is: " + objectArray.get(ageMinIndex));
-
-
-
-        }
+    //getters
+    public String getName() {
+        return this.name;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public Boolean getSearching() {
+        return searching;
+    }
+
+    public String toString() {                                           //replaces the hashcode object output with my own. not scalable to new fields
+        return "[" + name + "," + age + "," + city + "," + state + "," + zipCode + "," + industry + "," + searching + "]";
+    }
+}
+
 
